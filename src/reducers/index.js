5a4383b9644dcd87b576eff2ba25acca918f25ea
Romain niam0r <niamor@mm.st>
2018-02-21 17:59:59 +0000

    field.meta.touched ?

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
-import { PostsReducer } from './PostsReducer';
+import PostsReducer from './PostsReducer';
+//needs {           } potentially
 
 const rootReducer = combineReducers({
   posts: PostsReducer,
