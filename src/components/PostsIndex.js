fcdeb177be890f84036f38f93d4e48e09d39a670
Romain niam0r <niamor@mm.st>
2018-02-21 16:46:16 +0000

    installed redux-form@6.6.3

--- src/components/PostsIndex.js
+++ src/components/PostsIndex.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
--- src/components/PostsShow.js
+++ src/components/PostsShow.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class PostsShow extends Component {
   render() {
     return (
       <div>
+        <div className="text-xs-right">
+          <Link to="/" className="btn btn-primary">
+            Back to index
+          </Link>
+        </div>
         PostsShow
       </div>
     );
