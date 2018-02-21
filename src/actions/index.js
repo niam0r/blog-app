f2b8f5e1667707591fb3c07eb3f15d281661e8b6
Romain niam0r <niamor@mm.st>
2018-02-21 12:29:33 +0000

    all working up to lecture 128

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,9 @@
-import axios form 'axios';
+import axios from 'axios';
 
-export cont FETCH_POSTS = 'fetch_posts';
+export const FETCH_POSTS = 'fetch_posts';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=niam0r';
+const API_KEY = '?key=niam0r1234';
 
 export function fetchPosts () {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
