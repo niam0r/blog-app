bdaad473988500e7d68e7e305aaec3df7a14ce33
Romain niam0r <niamor@mm.st>
2018-02-21 11:09:06 +0000

    created PostsIndex PostsShow PostsNew

--- src/index.js
+++ src/index.js
@@ -19,11 +19,12 @@ class Goodbye extends React.Component {
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-      <BrowserRouter>
-        <div>
-          <Route path="/hello" component={Hello}/>
-          <Route path="/goodbye" component={Goodbye}/>
-        </div>
-      </BrowserRouter>
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+        <Route path="/posts/:id" component={PostsShow} />
+        <Route path="/posts/new" component={PostsNew} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
