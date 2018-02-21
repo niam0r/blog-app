9cc299903d2e7ee0375695545f8f0f10e98d5d8e
Romain niam0r <niamor@mm.st>
2018-02-21 18:10:25 +0000

    refactored meta.field with ES6

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -4,14 +4,20 @@ import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}}`;
+
     return (
-      <div className="form-group">
+      <div className="form-group has-danger">
         <label>{field.label}</label>
-        <input className="form-control"
+        <input
+          className="form-control"
           type="text"
           {...field.input}
         />
-        {field.meta.touched ? field.meta.error : ''}
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
       </div>
     );
   }
