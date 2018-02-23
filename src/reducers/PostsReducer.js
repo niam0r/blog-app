90791fe6478d9b66634b9ba04099a94bf09acd16
Romain niam0r <niamor@mm.st>
2018-02-23 17:36:26 +0000

    FETCH_POST in PostsReducer

--- src/reducers/PostsReducer.js
+++ src/reducers/PostsReducer.js
@@ -1,8 +1,15 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type) {
+  case FETCH_POST:
+    // const post = action.payload.data;
+    // const newState = { ...state };
+    // newState[post.id] = post;
+    // return newState
+    // ES6 equivalent of above 4 lines:
+    return { ...state, [action.payload.data.id]: action.payload.date };
   case FETCH_POSTS:
     return _.mapKeys(action.payload.data, 'id');
   default:
