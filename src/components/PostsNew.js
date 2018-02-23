6fe03bf8a2b4fbe28b73e437df7966e169f73b48
Romain niam0r <niamor@mm.st>
2018-02-23 16:30:46 +0000

    form validation implemented, review desctructuring, lecture 140 complete

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -5,10 +5,11 @@ import { Field, reduxForm } from 'redux-form';
 class PostsNew extends Component {
   renderField(field) {
     const { meta: { touched, error } } = field;
-    const className = `form-group ${touched && error ? 'has-danger' : ''}}`;
+    // review destructuring above
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
 
     return (
-      <div className="form-group has-danger">
+      <div className={className}>
         <label>{field.label}</label>
         <input
           className="form-control"
