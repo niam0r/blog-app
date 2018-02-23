10e387261d38025365a6616c4f48fc0f15612242
Romain niam0r <niamor@mm.st>
2018-02-23 17:51:00 +0000

    implemented PostShow, ownProps, review lecture 146

--- src/components/PostsShow.js
+++ src/components/PostsShow.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { fetchPost } from '../actions';
 
-export default class PostsShow extends Component {
+class PostsShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params; // grab post from the url
+    this.props.fetchPost(id);
+  }
+
   render() {
+
     return (
       <div>
         <div className="text-xs-right">
@@ -15,3 +23,9 @@ export default class PostsShow extends Component {
     );
   }
 }
+
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
