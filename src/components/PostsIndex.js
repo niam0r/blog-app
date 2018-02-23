ddb35e3d909552dd43381eb135292d6c4375e32e
Romain niam0r <niamor@mm.st>
2018-02-23 17:06:04 +0000

    fixed index (forgot mapStateToProps in connecr)

--- src/components/PostsIndex.js
+++ src/components/PostsIndex.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
 import _ from 'lodash';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchPosts } from '../actions';
@@ -12,18 +12,21 @@ class PostsIndex extends Component {
   renderPosts() {
     return _.map(this.props.posts, post => {
       return (
-        <li key={post.id} className="list-group-item">
-          {post.title}
+        <li className="list-group-item" key={post.id}>
+          <Link to={`/posts/${post.id}`}>
+            {post.title}
+          </Link>
         </li>
       );
     });
   }
 
+
   render() {
     return (
       <div>
         <div className="text-xs-right">
-          <Link to="/posts/new"className="btn btn-primary">
+          <Link className="btn btn-primary" to="/posts/new">
             Add a Post
           </Link>
         </div>
@@ -36,8 +39,8 @@ class PostsIndex extends Component {
   }
 }
 
-function mapStateToProps (state) {
+function mapStateToProps(state) {
   return { posts: state.posts };
 }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -27,8 +27,9 @@ class PostsNew extends Component {
   }
 
   onSubmit(values) {
-    this.props.history.push('/');
-    this.props.createPost(values);
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
   }
 
   render() {
