2072b48c9ca02c6f660e50635ae6c60d3e6e5574
Romain niam0r <niamor@mm.st>
2018-02-24 14:10:52 +0000

    _.omit to remove deleted post from state

--- src/reducers/PostsReducer.js
+++ src/reducers/PostsReducer.js
@@ -3,6 +3,9 @@ import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type) {
+  case DELETE_POST:
+    return _.omit(state, action.payload);
+    // return new state object without the deleted post
   case FETCH_POST:
     // const post = action.payload.data;
     // const newState = { ...state };
